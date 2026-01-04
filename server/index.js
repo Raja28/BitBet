3a73ff71b94a27a2590f2692c290a9401a851a61
Raja28 <rajadavid03@gmail.com>
2026-01-04 12:52:13 +0530

    Fix: Updated index.js

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ const cors = require("cors");
 app.use(express.json())
 const port = process.env.PORT || 2027;
 
-const { PrismaClient } = require('./prisma/generated');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', "https://bit-bet-server.vercel.app"],
     credentials: true
 }));
 
